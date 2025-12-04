d50552002bf4b44660f25654de4ee86d5966308e
Xalidaa <hacizadexalide@gmail.com>
2025-12-04 18:57:39 +0400

    Add CargoOrder class and Enums

---
+++ CffTask/Models/CargoOrder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CffTask.Enums;
+
+namespace CffTask.Models
+{
+    class CargoOrder
+    {
+        private static int Id { get; set; }
+
+        private int CustomerId { get; set; }
+
+        private int CourierId { get; set; }
+
+        public int TotalAmount { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Created;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        static CargoOrder()
+        {
+            Id = 0;
+            Id++;
+        }
+        public void UpdateStatus(OrderStatus newStatus)
+        {
+            if (newStatus == OrderStatus.Cancelled && Status == OrderStatus.Delivered)
+            {
+                throw new InvalidOperationException("Cannot cancel an order that has already been delivered.");
+            }
+
+            Status = newStatus;
+        }
+
+
+    }
+}
