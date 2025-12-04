f8dede88531b205500e62bde92157908961b7acf
ilham_huseyn <ilhamshh-bpa203@code.edu.az>
2025-12-04 19:27:57 +0400

    git ignore added

---
+++ PracticeCff/CffTask/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffTask
+{
+    public class Customer
+    {
+        private static int Id;
+        public string Name;
+        public string City;
+
+        public Customer(string name, string city)
+        {
+            Id++;
+            Name = name;
+            City = city;
+
+        }
+
+
+    }
+}
