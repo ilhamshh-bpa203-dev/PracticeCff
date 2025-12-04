bcd48b3797e200e56e6585f425cb88f03e410b72
ilhamshh-bpa203-dev <ilhamshh-bpa203@code.edu.az>
2025-12-04 18:55:07 +0400

    Customer and Courer class added

---
+++ CffTask/Courier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffTask
+{
+    public class Courier
+    {
+        private static int Id;
+        public string Name;
+        public bool IsAvailable = false;
+
+        public Courier(string name, bool isavailable)
+        {
+            Id++;
+            Name = name;
+            IsAvailable = isavailable;
+
+        }
+
+
+    }
+}
